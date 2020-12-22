afcc28b3cc91528cf3973c16bbce9d31274ec843
Chris O'Brien <chris.obrien@moat.co.uk>
2020-12-22 17:40:58 +0000

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    using Serilog;
+    using Serilog.Sinks.Datadog.Logs;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.DatadogLogs("<API_KEY>", configuration: new DatadogConfiguration { Url = "https://http-intake.logs.datadoghq.eu" })
+                .CreateLogger();
+
+            log.Information("Test");
+            log.Error("Test");
+        }
+    }
+}
